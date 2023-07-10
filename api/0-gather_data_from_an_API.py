773e7769f1592bb1220b80c0d4c793f2ee518471
Maxime <m.kwizera@alustudent.com>
2023-07-10 19:18:06 +0200

    a

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ Library to gather todo list from an API """
 
-import requests
-import sys
+import urllib.request
+import json
 
 """ script to retrieve data from an API """
 
